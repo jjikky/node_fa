ddc5ab393f4cae8ae33dd366aaa22f1d94c0ff9e
jjikky <kok5417@skuniv.ac.kr>
2024-03-21 16:28:16 +0900

    ✨ user crud api with typeorm

--- typeorm/src/index.ts
+++ typeorm/src/index.ts
@@ -1,6 +1,6 @@
 import { AppDataSource } from "./data-source";
 import { User } from "./entity/User";
-
+import { Request, Response } from "express";
 import express from "express";
 import morgan from "morgan";
 
@@ -30,6 +30,39 @@ AppDataSource.initialize()
     // );
   })
   .catch((error) => console.log(error));
+
+app.get("/users", async (req: Request, res: Response) => {
+  const user = await AppDataSource.getRepository(User).find();
+  res.json(user);
+});
+app.get("/users/:id", async (req: Request, res: Response) => {
+  const results = await AppDataSource.getRepository(User).findOneBy({
+    id: Number(req.params.id),
+  });
+  return res.json(results);
+});
+
+app.post("/users", async (req: Request, res: Response) => {
+  const user = await AppDataSource.getRepository(User).create(req.body);
+  console.log(user);
+  const results = await AppDataSource.getRepository(User).save(user);
+  return res.json(results);
+});
+
+app.put("/users/:id", async (req: Request, res: Response) => {
+  const user = await AppDataSource.getRepository(User).findOneBy({
+    id: Number(req.params.id),
+  });
+  AppDataSource.getRepository(User).merge(user, req.body);
+
+  const results = await AppDataSource.getRepository(User).save(user);
+  return res.json(results);
+});
+
+app.delete("/users/:id", async (req: Request, res: Response) => {
+  const results = await AppDataSource.getRepository(User).delete(req.params.id);
+  return res.json(results);
+});
 
 let port = 4000;
 
