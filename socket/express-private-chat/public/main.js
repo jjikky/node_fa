ad345b2550608b214bbe2f2ca418f6f8c6805d83
jjikky <kok5417@skuniv.ac.kr>
2024-03-23 21:37:34 +0900

    ✨ socket connect

---
+++ socket/express-private-chat/public/main.js
@@ -0,0 +1,19 @@
+const socket = io();
+// const socket = io("http://localhost:4000", {
+//   // connect after login
+//   autoConnect: false,
+// });
+
+socket.onAny((event, ...args) => {
+  console.log(event, ...args);
+});
+
+// 전역 변수들
+const chatBody = document.querySelector(".chat-body");
+const userTitle = document.querySelector("#user-title");
+const loginContainer = document.querySelector(".login-container");
+const userTable = document.querySelector(".users");
+const userTagline = document.querySelector("#users-tagline");
+const title = document.querySelector("#active-user");
+const messages = document.querySelector(".messages");
+const msgDiv = document.querySelector(".msg-form");
--- socket/express-private-chat/src/index.js
+++ socket/express-private-chat/src/index.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
+require("../db");
+const http = require("http");
+const { Server } = require("socket.io");
+const server = http.createServer(app);
+const io = new Server(server);
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
 app.use(express.json());
 
+let users = [];
+io.on("connection", async (socket) => {
+  console.log("connected socket");
+  let userData = {};
+  users.push(userData);
+  io.emit("users-data", { users });
+
+  // message from client
+  socket.on("message-to-server", () => {});
+
+  // get message from db
+  socket.on("fetch-messages", () => {});
+
+  socket.on("disconnect", () => {});
+});
+
 const port = 4000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
