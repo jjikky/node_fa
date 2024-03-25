3c2aeaf4ac018bd41532ee75440a546663a01cc6
jjikky <kok5417@skuniv.ac.kr>
2024-03-25 21:17:37 +0900

    ♻️ 파일 내용 교체, message model & util (save message) 작성

--- socket/express-private-chat/src/utils/messages.js
+++ socket/express-private-chat/src/utils/messages.js
@@ -1,27 +1,29 @@
-const { mongoose } = require("mongoose");
+const messageModel = require("../models/messages.model");
 
-const messageSchema = mongoose.Schema({
-  userToken: {
-    type: String,
-    required: true,
-  },
-  messages: [
+const getToken = (sender, receiver) => {
+  const key = [sender, receiver].sort().join("_");
+  return key;
+};
+
+const saveMessages = async ({ from, to, message, time }) => {
+  const token = getToken(from, to);
+  const data = {
+    from,
+    message,
+    time,
+  };
+  messageModel.updateOne(
+    { userToken: token },
     {
-      from: {
-        type: String,
-        required: true,
-      },
-      message: {
-        type: String,
-        required: true,
-      },
-      time: {
-        type: String,
-        required: true,
-      },
+      $push: { message: data },
     },
-  ],
-});
+    (err, res) => {
+      if (err) console.error(err);
+      console.log("메시지가 업데이트되었습니다.");
+    }
+  );
+};
 
-const messageModel = mongoose.model("Message", messageSchema);
-module.exports = messageModel;
+module.exports = {
+  saveMessages,
+};
